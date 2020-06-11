d0ca9c2a04a39d062d8acd4709e15559b19df7c8
dengguoqing <850625861@qq.com>
2020-06-11 14:37:10 +0800

    第4次提交代码到本地仓库。

---
+++ MongoDB/src/main/java/cn/itcast/mongodb_article/controller/testController.java
@@ -0,0 +1,13 @@
+package cn.itcast.mongodb_article.controller;
+
+/**
+ * @program: mongodb_aticle
+ * @ClassName testController
+ * @description:
+ * @author: DGQ
+ * @create: 2020-06-11 14:33
+ * @Version 1.0
+ **/
+public class testController {
+
+}
