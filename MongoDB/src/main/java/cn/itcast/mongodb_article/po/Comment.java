af47d1156c60e0f88517a747812e57d8c9d6bcfd
dengguoqing <850625861@qq.com>
2021-07-16 22:01:27 +0800

    增加test类，修改为public

--- MongoDB/src/main/java/cn/itcast/mongodb_article/po/Comment.java
+++ MongoDB/src/main/java/cn/itcast/mongodb_article/po/Comment.java
@@ -26,7 +26,7 @@ import java.util.Date;
 public class Comment implements Serializable {
     //主键标识，该属性的值会自动对应mongodb的主键字段"_id"，如果该属性名就叫“id”,则该注解可以省略，否则必须写
     @Id
-    static private String id;//主键
+    static public  String id;//主键
     private String articleid;//文章id
     //该属性对应mongodb的字段的名字，如果一致，则无需该注解
     @Field("content")
---
+++ MongoDB/src/main/java/cn/itcast/mongodb_article/po/Test.java
@@ -0,0 +1,8 @@
+package cn.itcast.mongodb_article.po;
+
+/**
+ * create by dengguoqing on 2021-07-16 21:58
+ **/
+public class Test {
+
+}
